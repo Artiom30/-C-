68e882ec1103633ecb5ffd7ca67139be0f85a6c1
Artiom30 <mezheynikov1@gmail.com>
2022-10-04 20:39:20 +0300

    1

--- Seminars_C#/Seminar_2/Classwork.cs
+++ Seminars_C#/Seminar_2/Classwork.cs
@@ -1,1 +1,23 @@
-﻿
+﻿// Задача 1.Напишите программу, которая принимает двузначное число и показывает
+//          наибольшую цифру числа.
+
+int FindBiggerDiget(int num);
+{
+    int result;
+
+    int ed = num % 10;
+    int dec = num / 10;
+
+    if (ed > dec)
+        result = ed;
+    else
+        result = des;
+
+
+    return result;
+}
+Console.Write("Imput two-digit number:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int BiggerDigit = FindBiggerDiget(number);
+Console.WriteLine($"Bigger diget of {number} is {BiggerDigit}");
