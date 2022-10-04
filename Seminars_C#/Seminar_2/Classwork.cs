ad592f7712bb465928c7cde58268ad8ef2660959
Artiom30 <mezheynikov1@gmail.com>
2022-10-04 21:44:19 +0300

    04.10.22Seminar2

--- Seminars_C#/Seminar_2/Classwork.cs
+++ Seminars_C#/Seminar_2/Classwork.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 1.Напишите программу, которая принимает двузначное число и показывает
 //          наибольшую цифру числа.
-
+/*
 int FindBiggerDiget(int num);
 {
     int result;
@@ -21,3 +21,34 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 int BiggerDigit = FindBiggerDiget(number);
 Console.WriteLine($"Bigger diget of {number} is {BiggerDigit}");
+*/
+
+//  Задача 2.Напишите программу, которая выводит случайное трёхзначное
+//           число и удаляет вторую цифру этого числа.
+
+int DeleteSecondNum(int num)
+{
+    int result;
+
+    int ed = num % 10;
+    int sot = num / 10;
+
+    result = sot*10 + ed; 
+    return result;  
+}
+
+int number = new Random().Next(100,999);
+int number = new num; 
+
+int twoBigitNumber = DeleteSecondNumber(number);
+Console.WriteLine($"a three-digit {number} without a second cipher {twoBigitNumber}
+");
+
+
+
+//  Задача 3. Напишите программу, которая принимает на вход число n и проверяет, кратно ли 
+//            оно одновременно a и b(целочисленные делители, также задаются пользователем).
+
+
+
+
