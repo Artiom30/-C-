541665f9fb3506924637988a664466bea5cb5a63
Artiom30 <mezheynikov1@gmail.com>
2022-10-05 20:00:42 +0300

    fixed issue 1 and figured it out

--- Seminars_C#/Seminar_2/Classwork.cs
+++ Seminars_C#/Seminar_2/Classwork.cs
@@ -1,31 +1,37 @@
-﻿// Задача 1.Напишите программу, которая принимает двузначное число и показывает
-//          наибольшую цифру числа.
+﻿// Задача 1.Напишите программу, которая принимает двузначное число 
+//          и показывает наибольшую цифру числа.
 /*
-int FindBiggerDiget(int num);
+int FindBiggerDiget(int num)
 {
     int result;
-
-    int ed = num % 10;
-    int dec = num / 10;
-
-    if (ed > dec)
-        result = ed;
+    if (num < 10 || num >= 100)
+        return 0;
     else
-        result = des;
+    {
+        int ed = num % 10;
+        int dec = num / 10;
 
-
-    return result;
+        if (ed > dec)
+        {
+            result = ed;
+        }
+        else
+        {
+            result = dec;
+        }
+        return result;
+        }
 }
-Console.Write("Imput two-digit number:");
-int number = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Imput two-digit number:");
+    int number = Convert.ToInt32(Console.ReadLine());
 
-int BiggerDigit = FindBiggerDiget(number);
-Console.WriteLine($"Bigger diget of {number} is {BiggerDigit}");
+    int BiggerDigit = FindBiggerDiget(number);
+    Console.WriteLine($"Bigger diget of {number} is {BiggerDigit}");
 */
 
 //  Задача 2.Напишите программу, которая выводит случайное трёхзначное
 //           число и удаляет вторую цифру этого числа.
-
+/*
 int DeleteSecondNum(int num)
 {
     int result;
@@ -43,7 +49,7 @@ int number = new num;
 int twoBigitNumber = DeleteSecondNumber(number);
 Console.WriteLine($"a three-digit {number} without a second cipher {twoBigitNumber}
 ");
-
+*/
 
 
 //  Задача 3. Напишите программу, которая принимает на вход число n и проверяет, кратно ли 
