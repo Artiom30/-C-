5280738c8872ed3a2abb3830b88c3246ec619448
Artiom30 <mezheynikov1@gmail.com>
2022-10-08 11:30:13 +0300

    HWTask2

--- Seminars_C#/Seminar_2/HomeWork.cs
+++ Seminars_C#/Seminar_2/HomeWork.cs
@@ -4,7 +4,7 @@
 //              456 -> 5
 //              782 -> 8
 //              918 -> 1
-
+/*
 int SecondNumber(int num)
 {
     int result = 0;
@@ -29,3 +29,40 @@ if (Result == 0)
    Console.WriteLine("Your number does not match the condition of the problem.");
 else
 Console.WriteLine($"Second digit from the number:{res} is {Result}");
+*/
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного
+//            числа или сообщает, что третьей цифры нет.
+
+//            645 -> 5
+//            78 -> третьей цифры нет
+//            32679 -> 6
+
+int TreeDigNum(int num)
+{
+    int result = 11;
+    int current = num;
+
+    if (current > 99)
+    {
+        while (current > 1000) 
+            current = current / 10;
+        {
+            result = current % 10;
+            return result;
+        }
+    }
+    else
+        return result;
+}
+
+Console.WriteLine("Imput your digit: ");
+double num = Convert.ToDouble(Console.ReadLine());
+int Num = Convert.ToInt32(Math.Round(num,0));     // это конечно хорошо, но какого оно округляет 99,6 до 100...
+
+int result = TreeDigNum(Num);
+
+if (result == 11)
+    Console.WriteLine("Your number does not have a third digit.");
+else
+    Console.WriteLine($"The third digit of your number is {result}.");
