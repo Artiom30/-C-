712f04c5d521c359fe04a114abcd0457c8a11c3c
Artiom30 <mezheynikov1@gmail.com>
2022-10-17 23:20:14 +0300

    Homework5 Task2

--- Seminars_C#/Seminar_5/HomeWork.cs
+++ Seminars_C#/Seminar_5/HomeWork.cs
@@ -1,4 +1,4 @@
-// Задача 34:  Задайте массив заполненный случайными положительными трёхзначными 
+// Задача 1:  Задайте массив заполненный случайными положительными трёхзначными 
 //             числами. Напишите программу, которая покажет количество чётных чисел в 
 //             массиве.
 //             [345, 897, 568, 234] -> 2
@@ -28,4 +28,39 @@ int size = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Here is an array of three digit numbers.");
 int[] array = ThreeDigArray(size);
 CounterTwoDigNum(array);
+*/
+
+// Задача 2: Задайте одномерный массив, заполненный случайными числами. Найдите
+//            сумму элементов, стоящих на нечётных позициях.
+//            [3, 7, 23, 12] -> 19
+//            [-4, -6, 89, 6] -> 0
+/*
+int[] RandomArr(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 100);
+    return array;
+}
+
+void SumOfOddIndex(int[] array)
+{
+    int sumOfNum = 0;
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0 || array[i] / 10 > 0) Console.Write($" {i} ");
+        else Console.Write(i + " ");
+        if (i % 2 == 1) sumOfNum += array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine("The sum of all numbers in odd positions = " + sumOfNum);
+}
+
+Console.WriteLine("ENTER a size of array:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = RandomArr(size);
+SumOfOddIndex(array);
 */
