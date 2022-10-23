7077410a72509cd321cb463bd56ca6534311a5c9
Artiom30 <mezheynikov1@gmail.com>
2022-10-23 21:13:22 +0300

    Homework7

--- Seminars_C#/Seminar_7/Homework.cs
+++ Seminars_C#/Seminar_7/Homework.cs
@@ -1,23 +1,23 @@
-int[,] CreateRandom2DArray()
-{
-    Console.WriteLine("Imput number of rows: ");
-    int rows = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Imput number of columns: ");
-    int columns = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Imput a min possible value: ");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Imput a max possible value: ");
-    int maxValue = Convert.ToInt32(Console.ReadLine());
+// int[,] CreateRandom2DArray()
+// {
+//     Console.WriteLine("Imput number of rows: ");
+//     int rows = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Imput number of columns: ");
+//     int columns = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Imput a min possible value: ");
+//     int minValue = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Imput a max possible value: ");
+//     int maxValue = Convert.ToInt32(Console.ReadLine());
 
-    int[,] array = new int[rows, columns];
+//     int[,] array = new int[rows, columns];
 
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < columns; j++)
-            array[i, j] = new Random().Next(minValue, maxValue + 1);
-    return array;
-}
+//     for (int i = 0; i < rows; i++)
+//         for (int j = 0; j < columns; j++)
+//             array[i, j] = new Random().Next(minValue, maxValue + 1);
+//     return array;
+// }
 
-void Show2DArray(int[,] array)
+// void Show2DArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
