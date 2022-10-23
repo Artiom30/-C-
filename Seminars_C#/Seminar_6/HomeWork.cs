0c624bce81575f66e157110d8df010b080d18877
Artiom30 <mezheynikov1@gmail.com>
2022-10-23 18:18:11 +0300

    Homework 6

--- Seminars_C#/Seminar_6/HomeWork.cs
+++ Seminars_C#/Seminar_6/HomeWork.cs
@@ -21,8 +21,53 @@ void CounterNumbers()
 CounterNumbers();
 */
 
-
+/*
 // Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
 //           заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1,
 //           k1, b2 и k2 задаются пользователем.
 //           b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// y = k1 * x + b1
+// y = k2 * x + b2
+
+// y - y = k1 * x + b1 - k2 * x - b2
+
+// 0 = k1 * x - k2 * x + b1 - b2
+
+// -k1 * x + k2 * x = b1 - b2
+
+// ( -k1 + k2) * x = b1 - b2
+
+// x = (b1 - b2) / (-k1 + k2)   -   x = (2 - 4)/(-5 + 9) = -0,5
+
+// y = k1 * ((b1 - b2) / (-k1 + k2)) + b1    -   y = 5 * ((2 - 4)/(-5 + 9)) + 2 = -0,5
+//             или                                      одинаково
+// y = k2 * ((b1 - b2) / (-k1 + k2)) + b2    -   y = 9 * ((2 - 4)/(-5 + 9)) + 4 = -0,5
+// Так и подставлю в программу.
+
+void IntersectionCoordinates(double b1, double b2,double k1, double k2)
+{
+    double x = (b1 - b2) / (-k1 + k2);
+    double y = k1 * ((b1 - b2) / (-k1 + k2)) + b1;
+    Console.WriteLine($"Coordinates of intersection points in a 2D plane is ({Math.Round(x,1)} , {Math.Round(y,1)})");
+}
+
+Console.WriteLine("y = k1 * x + b1 and y = k2 * x + b2");
+Console.WriteLine("These are the equations of lines in 2D space.");
+Console.WriteLine("Enter the values of the variables and I will find the coordinates of the points.");
+Console.WriteLine("b1 = ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("b2 = ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("k1 = ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("k2 = ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+IntersectionCoordinates(b1,b2,k1,k2);
+*/
+
+// Решить то решил. А что за b1,b2,k1,k2 и откуда они возникают,
+// и где ваобще это можно использовать... 
+
+
