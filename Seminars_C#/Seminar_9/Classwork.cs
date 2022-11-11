9b4ce457f2acd7a3459f480def7f265381ae42ef
Artiom30 <mezheynikov1@gmail.com>
2022-11-11 19:45:02 +0300

    Seminar 9

--- Seminars_C#/Seminar_9/Classwork.cs
+++ Seminars_C#/Seminar_9/Classwork.cs
@@ -1,2 +1,65 @@
 ﻿// Семинар 9
-// Тема:
+// Тема: Рекурсия. Рекурсивные методы.
+
+// Задача 1: Задайте значение N. Напишите программу, которая выведет все натуральные 
+//           числа в промежутке от 1 до N.
+/*
+void ShowNums(int n)
+{
+    if(n > 1) ShowNums(n - 1);
+    Console.Write(n + " ");
+}
+
+ShowNums(5);
+*/
+
+// Задача 2: Напишите программу, которая будет принимать на вход число и возвращать 
+//           сумму его цифр.
+/*
+int SumOfDigits(int n)
+{
+    if (n != 0) return SumOfDigits(n / 10) + n % 10;
+    else return 0;
+}
+
+Console.WriteLine(SumOfDigits(1234));
+*/
+// Задача 3:  Задайте значения M и N. Напишите программу, которая выведет все
+//            натуральные числа в промежутке от M до N.
+/*
+void NumberMtoN (int m, int n)
+{
+    if (m == n)
+        Console.Write(n + " ");
+    if (n < m)
+    {
+        NumberMtoN(m-1,n);
+        Console.Write(m + " ");
+    }
+    if (n > m) 
+    {
+        NumberMtoN(m,n-1);
+        Console.Write(n + " ");
+    }
+}
+
+NumberMtoN(5,1);
+*/
+// Задача 4 : Напишите программу, которая на вход принимает два числа A и B,
+//            и возводит число А в целую степень B.
+/*
+double ADegreeB(double a, double b)
+{
+    if (b < 0)
+    {
+        return 1 / a * ADegreeB(a, b + 1);
+    }
+    if (b > 0)
+    {
+        return a * ADegreeB(a, b - 1);
+    }
+    return 1;
+}
+
+Console.WriteLine(ADegreeB(2, -3));
+*/
